9606e33b0fde760355f13dc9bd9059634799e53a
Hafimuthasir <muthasirhafi@gmail.com>
2022-10-15 09:45:22 +0530

    cartcount

--- app_ga/contextprocessor.py
+++ app_ga/contextprocessor.py
@@ -6,10 +6,13 @@ def counter(request):
     cart_count = 0
     if 'username' in request.session:
         Cart = cart.objects.all()
+        
         wish = wishlist.objects.all()
-        cart_count = len(Cart)
         wish = len(wish)
+        cart_count = len(Cart)
+        
     else :
+        wish = None
         gcart = guest_cart2.objects.all()
         cart_count = len(gcart)
     return dict(cart_count=cart_count,wish=wish)
