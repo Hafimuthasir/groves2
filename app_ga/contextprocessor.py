af77a2ba768278d1f0ffd0e495657d46fb5e908d
Hafimuthasir <muthasirhafi@gmail.com>
2023-02-03 12:25:30 +0530

    'place_order_bt_and_cart_count_fix'

--- app_ga/contextprocessor.py
+++ app_ga/contextprocessor.py
@@ -1,19 +1,24 @@
 from app_ga.models import guest_cart2, wishlist
-from app_ga.models import cart
+from app_ga.models import cart,myusers
 
 
 def counter(request):
     cart_count = 0
     if 'username' in request.session:
-        Cart = cart.objects.all()
+        username = request.session.get('username')
+        user=myusers.objects.get(username=username)
+        cartitems=cart.objects.filter(userid = user.id)
+        print('lollllllllllll',cartitems)
         
-        wish = wishlist.objects.all()
+        wish = wishlist.objects.filter(userid = user.id)
         wish = len(wish)
-        cart_count = len(Cart)
+        cart_count = len(cartitems)
         
     else :
         wish = 0
-        gcart = guest_cart2.objects.all()
-        cart_count = len(gcart)
+        if 'guest' in request.session:
+            session_key = request.session.get('guest')
+            gcart = guest_cart2.objects.filter(user_session = session_key)
+            cart_count = len(gcart)
     return dict(cart_count=cart_count,wish=wish)
 
